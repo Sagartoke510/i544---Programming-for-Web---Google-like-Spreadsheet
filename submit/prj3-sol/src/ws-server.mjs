11bc2707dca8c98bd223d432827b62b46e6751dd
Sagar S Toke <stoke1@CS444-stoke1.pods.bu.int>
2020-10-31 23:50:05 -0400

    replace and update functionality added

--- submit/prj3-sol/src/ws-server.mjs
+++ submit/prj3-sol/src/ws-server.mjs
@@ -1,9 +1,9 @@
-import assert from 'assert';
-import cors from 'cors';
-import express from 'express';
-import bodyParser from 'body-parser';
+import assert from "assert";
+import cors from "cors";
+import express from "express";
+import bodyParser from "body-parser";
 
-import {AppError} from 'cs544-ss';
+import { AppError } from "cs544-ss";
 
 /** Storage web service for spreadsheets.  Will report DB errors but
  *  will not make any attempt to report spreadsheet errors like bad
@@ -26,41 +26,39 @@ export default function serve(port, ssStore) {
   app.locals.port = port;
   app.locals.ssStore = ssStore;
   setupRoutes(app);
-  app.listen(port, function() {
+  app.listen(port, function () {
     console.log(`listening on port ${port}`);
   });
 }
 
 const CORS_OPTIONS = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204,
-  exposedHeaders: 'Location',
+  exposedHeaders: "Location",
 };
 
-const BASE = 'api';
-const STORE = 'store';
-
+const BASE = "api";
+const STORE = "store";
 
 function setupRoutes(app) {
-  app.use(cors(CORS_OPTIONS));  //needed for future projects
+  app.use(cors(CORS_OPTIONS)); //needed for future projects
   //@TODO add routes to handlers
   //const base = app.locals.base;
   //app.use(cors());
   app.use(bodyParser.json());
   app.get(`/${BASE}/${STORE}/:SS_NAME`, doGet(app));
-  app.put(`/${BASE}/${STORE}/:SS_NAME`, doReplaceAll(app));
+  app.put(`/${BASE}/${STORE}/:SS_NAME`, doReplace(app));
   app.patch(`/${BASE}/${STORE}/:SS_NAME`, doUpdate(app));
-  app.delete(`${base}/${STORE}/:SS_NAME`, doClear(app));
-  app.put(`/${BASE}/${STORE}/:SS_NAME/:CELL_ID`, doReplaceCell(app));
-  app.patch(`/${BASE}/${STORE}/:SS_NAME/:CELL_ID`, doUpdateCell(app));
-  app.delete(`/${BASE}/${STORE}/:SS_NAME/:CELL_ID`,doDeleteCell(app));
+  app.delete(`/${BASE}/${STORE}/:SS_NAME`, doClear(app));
+  app.put(`/${BASE}/${STORE}/:SS_NAME/:CELL_ID`, doReplace(app));
+  app.patch(`/${BASE}/${STORE}/:SS_NAME/:CELL_ID`, doUpdate(app));
+  app.delete(`/${BASE}/${STORE}/:SS_NAME/:CELL_ID`, doDeleteCell(app));
 
   //must be last
   app.use(do404(app));
   app.use(doErrors(app));
-}
 }
 
 /****************************** Handlers *******************************/
@@ -68,141 +66,182 @@ function setupRoutes(app) {
 //@TODO
 
 function doGet(app) {
-  return (async function (req, res){
+  return async function (req, res) {
     try {
       const ssName = req.params.SS_NAME;
-      const results = await app.locals.ssStore.readFormula(ssName);
+      const results = await app.locals.ssStore.readFormulas(ssName);
       res.json(results);
-    }
-    catch(err) {
+    } catch (err) {
       const mapped = mapError(err);
       res.status(mapped.status).json(mapped);
     }
-  });
+  };
 }
 
-function doReplaceAll(app)  {
-  return (async function (req, res){
-    try {
-      const ssName = req.params.SS_NAME;
-
-    }
-    catch(err)  {
-      const mapped = mapError(err);
-      res.status(mapped.status).json(mapped);
-    }
-  });
-}
-
-function doUpdate(app)  {
-  return (async function (req, res){
-    try {
-
-    }
-    catch(err)  {
-      const mapped = mapError(err);
-      res.status(mapped.status).json(mapped);
-    }
-  });
-}
-
-function doClear(app)  {
-  return (async function (req, res){
-    try {
-      const ssName = req.params.SS_NAME;
-      const results = await app.locals.ssStore.clear(ssName);
-      res.sendStatus(NO_CONTENT);
-    }
-    catch(err)  {
-      const mapped = mapError(err);
-      res.status(mapped.status).json(mapped);
-    }
-  });
-}
-
-function doReplaceCell(app)  {
-  return (async function (req, res){
+function doReplace(app) {
+  return async function (req, res) {
     try {
       const replacement = Object.assign({}, req.body);
       const ssName = req.params.SS_NAME;
       const cellId = req.params.CELL_ID;
-      const results = await app.locals.ssStore.updateCell(ssName, cellId, replacement['formula']);
+      console.log(req.body);
+      if (cellId) {
+        if (!checkJsonAsObject(req.body)) {
+          throw new AppError(
+            'BAD_REQUEST',
+            'request body must be a { formula } object'
+          );
+        }
+
+        await app.locals.ssStore.updateCell(
+          ssName,
+          cellId,
+          replacement['formula']
+        );
+      } else {
+        console.log(checkJsonAsList(req.body));
+        if (!checkJsonAsList(req.body)) {
+          throw new AppError(
+            'BAD_REQUEST',
+            'request body must be a list of cellId, formula pairs'
+          );
+        }
+        const clear = await app.locals.ssStore.clear(ssName);
+        for (const value of req.body) {
+          const results = app.locals.ssStore.updateCell(
+            ssName,
+            value[0],
+            value[1]
+          );
+        }
+      }
       res.sendStatus(CREATED);
-    }
-    catch(err)  {
+    } catch (err) {
       const mapped = mapError(err);
       res.status(mapped.status).json(mapped);
     }
-  });
+  };
 }
 
-function doUpdateCell(app)  {
-  return (async function (req, res){
+function doUpdate(app) {
+  return async function (req, res) {
     try {
       const patch = Object.assign({}, req.body);
       const ssName = req.params.SS_NAME;
       const cellId = req.params.CELL_ID;
-
-      const results = app.locals.model.updateCell(ssName, cellId, replacement['formula']);
-      res.sendStatus(OK);
-
-    }
-    catch(err)  {
+      if (cellId) {
+        if (!checkJsonAsObject(req.body)) {
+          throw new AppError(
+            'BAD_REQUEST',
+            'request body must be a { formula } object'
+          );
+        }
+        const results = app.locals.ssStore.updateCell(
+          ssName,
+          cellId,
+          patch['formula']
+        );
+      } else {
+        if (!checkJsonAsList(req.body)) {
+          throw new AppError(
+            'BAD_REQUEST',
+            'request body must be a list of cellId, formula pairs'
+          );
+        }
+        for (const value of req.body) {
+          const results = app.locals.ssStore.updateCell(
+            ssName,
+            value[0],
+            value[1]
+          );
+        }
+      }
+      res.sendStatus(NO_CONTENT);
+    } catch (err) {
       const mapped = mapError(err);
       res.status(mapped.status).json(mapped);
     }
-  });
+  };
 }
 
-function doDeleteCell(app)  {
-  return (async function (req, res){
+function doClear(app) {
+  return async function (req, res) {
+    try {
+      const ssName = req.params.SS_NAME;
+      const results = await app.locals.ssStore.clear(ssName);
+      res.sendStatus(NO_CONTENT);
+    } catch (err) {
+      const mapped = mapError(err);
+      res.status(mapped.status).json(mapped);
+    }
+  };
+}
+
+function doDeleteCell(app) {
+  return async function (req, res) {
     try {
       const ssName = req.params.SS_NAME;
       const cellId = req.params.CELL_ID;
       const results = await app.locals.ssStore.delete(ssName, cellId);
       res.sendStatus(NO_CONTENT);
-    }
-    catch(err)  {
+    } catch (err) {
       const mapped = mapError(err);
       res.status(mapped.status).json(mapped);
     }
-  });
+  };
+}
+
+function checkJsonAsList(body) {
+  for (const value of body) {
+    console.log(value);
+    if(value instanceof Array){
+    if (value.length === 2) {
+      return true;
+    }
+  }
+    else
+      return false;
+  }
+
+}
+
+function checkJsonAsObject(body) {
+  if (body instanceof Object) {
+    if ('formula' in body) return true;
+  }
+  return false;
 }
 /** Default handler for when there is no route for a particular method
  *  and path.
  */
 function do404(app) {
-  return async function(req, res) {
+  return async function (req, res) {
     const message = `${req.method} not supported for ${req.originalUrl}`;
     const result = {
       status: NOT_FOUND,
-      error: { code: 'NOT_FOUND', message, },
+      error: { code: "NOT_FOUND", message },
     };
-    res.status(404).
-	json(result);
+    res.status(404).json(result);
   };
 }
 
-
 /** Ensures a server error results in nice JSON sent back to client
  *  with details logged on console.
- */ 
+ */
 function doErrors(app) {
-  return async function(err, req, res, next) {
+  return async function (err, req, res, next) {
     const result = {
       status: SERVER_ERROR,
-      error: { code: 'SERVER_ERROR', message: err.message },
+      error: { code: "SERVER_ERROR", message: err.message },
     };
     res.status(SERVER_ERROR).json(result);
     console.error(err);
   };
 }
 
-
 /*************************** Mapping Errors ****************************/
 
-const ERROR_MAP = {
-}
+const ERROR_MAP = {};
 
 /** Map domain/internal errors into suitable HTTP errors.  Return'd
  *  object will have a "status" property corresponding to HTTP status
@@ -210,20 +249,18 @@ const ERROR_MAP = {
  *  message properties.
  */
 function mapError(err) {
-  const isDomainError = (err instanceof AppError);
-  const status =
-    isDomainError ? (ERROR_MAP[err.code] || BAD_REQUEST) : SERVER_ERROR;
-  const error = 
-	isDomainError
-	? { code: err.code, message: err.message } 
-        : { code: 'SERVER_ERROR', message: err.toString() };
+  const isDomainError = err instanceof AppError;
+  const status = isDomainError
+    ? ERROR_MAP[err.code] || BAD_REQUEST
+    : SERVER_ERROR;
+  const error = isDomainError
+    ? { code: err.code, message: err.message }
+    : { code: "SERVER_ERROR", message: err.toString() };
   if (!isDomainError) console.error(err);
   return { status, error };
-} 
+}
 
 /****************************** Utilities ******************************/
-
-
 
 /** Return original URL for req */
 function requestUrl(req) {
